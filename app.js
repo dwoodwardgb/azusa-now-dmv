f163f5b7e0bd73639d9881030bd6b26b1fa890de
David Woodward <dwoodwardgb@gmail.com>
2016-04-24 19:45:03 -0400

    Ready to add security and style

--- app.js
+++ app.js
@@ -16,26 +16,47 @@ const port = 3000;
 const staticDirName = 'public';
 
 app.use(logger('dev')); // logger
-app.use(bodyParser.urlencoded({ extended: false })); // url body parser
+app.use(bodyParser.urlencoded({ extended: true })); // url body parser
 app.use(express.static(path.join(__dirname, staticDirName))); // static dir
+
+// for use with checkbox inputs
+function checkboxToBool(obj, propName) {
+  if (obj[propName] === 'on') {
+    obj[propName] = true;
+  } else {
+    obj[propName] = false;
+  }
+}
 
 // routing
 app.post('/', (req, res, next) => { // for saving data
   console.log(req.params);
   console.log(req.body);
 
+  // add the community groups field if missing
+  if (!req.body.communityGroups) {
+    req.body.communityGroups = [];
+  }
+
+  checkboxToBool(req.body, 'goesToChurch');
+  checkboxToBool(req.body, 'partOfGroup');
+  req.body.communityGroups.forEach(
+    (group) => checkboxToBool(group, 'leader'));
+
+  res.send('success!');
+
   // TODO ???
 
-  const collectionName = 'people';
-  mydb((db) => {
-    db.collection(collectionName).insertOne(req.body, (err) => {
-      if (err) next(err);
-      else res.send('success!');
-    });
-  }, () => {
-    // error, not connected to database
-    next();
-  });
+  // const collectionName = 'people';
+  // mydb((db) => {
+  //   db.collection(collectionName).insertOne(req.body, (err) => {
+  //     if (err) next(err);
+  //     else res.send('success!');
+  //   });
+  // }, () => {
+  //   // error, not connected to database
+  //   next();
+  // });
 
 });
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,107 @@
-/* jshint browser: true */
-/* jshint jquery: true */
-
 'use strict';
 
-$(document).ready(() => {
+function synchCheckboxAndDivVisibility(checkbox, div) {
+  if (checkbox.is(':checked')) {
+    div.show();
+  } else {
+    div.hide();
+  }
+}
+
+$(document).ready(function () {
+
+  // states select templating --------------------------------------------------
+
+  var statesSelectTemplate = $('#statesSelectTemplate');
+
+  function renderStatesSelectTemplate(toRender) {
+    var newSelect = statesSelectTemplate.clone();
+    newSelect.removeAttr('id');
+    newSelect.removeAttr('hidden');
+    newSelect.attr('name', toRender.attr('data-name'));
+    toRender.append(newSelect);
+    toRender.removeAttr('data-name');
+    return newSelect;
+  }
+
+  $('.statesSelect').each(function (_, element) {
+    renderStatesSelectTemplate($(element));
+  });
+
+  // church form logic ---------------------------------------------------------
+
+  var churchCheckbox = $('#goesToChurchCheckbox');
+  var churchForm = $('#churchForm');
+
+  churchCheckbox.click(function () {
+    churchForm.toggle('hidden');
+  });
+
+  synchCheckboxAndDivVisibility(churchCheckbox, churchForm);
+
+  // community group form logic ------------------------------------------------
+
+  var communityGroupTemplate = $('#communityGroupTemplate');
+
+  function CommunityGroupsManager(container) {
+    var count = 0;
+
+    function add() {
+      var newGroup = communityGroupTemplate.clone();
+      newGroup.removeAttr('id');
+      newGroup.removeAttr('hidden');
+      newGroup.find('select').addClass('changeName');
+
+      newGroup.find('.changeName').each(function (_, element) {
+        element = $(element);
+        var newName = element.attr('name').replace('[i]', '[' + count + ']');
+        element.attr('name', newName);
+        element.removeClass('changeName');
+      });
+
+      newGroup.find('.remover').click(function () {
+        newGroup.remove();
+      });
+
+      container.append(newGroup);
+      count += 1;
+    }
+
+    return {
+      add: add
+    };
+  }
+
+  var communityGroupCheckbox = $('#partOfCommunityGroupCheckbox');
+  var communityGroupForms = $('#communityGroupForms');
+  var communityGroupFormsContainer = $('#communityGroupFormsContainer');
+
+  var communityGroupsManager = CommunityGroupsManager(communityGroupFormsContainer);
+  var groupsAddBtn = $('#addCommunityGroupBtn');
+
+  groupsAddBtn.click(function () {
+    communityGroupsManager.add();
+  });
+
+  communityGroupCheckbox.click(function () {
+    communityGroupForms.toggle('hidden');
+  });
+
+  communityGroupsManager.add();
+
+  synchCheckboxAndDivVisibility(communityGroupCheckbox, communityGroupForms);
+
+  // form submit logic ---------------------------------------------------------
+
+  var form = $('form');
+  form.submit(function () {
+    if (!communityGroupCheckbox.is(':checked')) {
+      communityGroupFormsContainer.remove();
+    }
+
+    return true;
+  });
+
+  // ---------------------------------------------------------------------------
+
 });
